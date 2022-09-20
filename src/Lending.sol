a8f084e80226cc1fc6483e9883e5f3709af65f30
procfs <woosun.song@dream-academy.io>
2022-09-20 10:37:43 +0900

    initial commit

---
+++ src/Lending.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Counter {
+   function deposit(address tokenAddress, uint256 amount) public {
+
+   }
+    function borrow(address tokenAddress, uint256 amount) public {
+
+    }
+    function repay(address tokenAddress, uint256 amount) public {
+
+    }
+    function liquidate(address user, address tokenAddress, uint256 amount) public {
+
+    }
+    function withdraw(address tokenAddress, uint256 amount) public {
+        
+    }
+}
